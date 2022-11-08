6c443acceadf7a24265f397f868220e8b168cb95
Michael Ovdiienko <mishaov@gmail.com>
2022-11-09 00:15:12 +0200

    feat: add slider in modal, made header button clickable, add anchors in the menu

--- javascript/main.js
+++ javascript/main.js
@@ -26,3 +26,28 @@ const swiper = new Swiper(".swiper", {
   slidesPerView: 2,
   spaceBetween: 15,
 });
+
+const swiperInFirstModal = new Swiper(".swiperInFirstModal", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+
+  slidesPerView: 1,
+  spaceBetween: 15,
+});
