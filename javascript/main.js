d28ea08cd75f7f3c8ffc7e8a56227db58aa38a1d
Michael Ovdiienko <mishaov@gmail.com>
2022-11-08 23:35:14 +0200

    feat: initialize repository

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  $(".header").height($(window).height());
+});
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+
+  slidesPerView: 2,
+  spaceBetween: 15,
+});
